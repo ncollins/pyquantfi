74e4cfd92ba375209c49d710b048cda3415fb570
Nick Collins <nick@vividx.co.uk>
2012-03-12 20:02:06 -0400

    streamline payoffs.py

--- payoffs.py
+++ payoffs.py
@@ -1,13 +1,7 @@
 # PyQuantFi - payoffs.py
 # (c) 2012 Nick Collins
 
-import math
-import random
-
-class Payoff(object):
-    0
-
-class VanillaCall(Payoff):
+def VanillaCall(k):
     """
     >>> p = VanillaCall(100)
     >>> (p(150),p(100),p(50),p(0))
@@ -15,15 +9,12 @@ class VanillaCall(Payoff):
     >>> 0 == round(p(100.00001) - 0.00001,5)
     True
     """
-    
-    def __init__(self,k):
-        self._strike = k
-
-    def __call__(self,s):
-        return max(s - self._strike, 0)
+    def call(s):
+        return max(s - k, 0)
+    return call
 
 
-class VanillaPut(Payoff):
+def VanillaPut(k):
     """
     >>> p = VanillaPut(100)
     >>> (p(150),p(100),p(50),p(0))
@@ -31,40 +22,31 @@ class VanillaPut(Payoff):
     >>> 0 == round(0.00001 - p(99.99999),5)
     True
     """
+    def call(s):
+        return max(k - s, 0)
+    return call
 
-    def __init__(self,k):
-        self._strike = k
-        
-    def __call__(self,s):
-        return max(self._strike - s, 0)
-
-class DigitalCall(Payoff):
+def DigitalCall(k):
     """
     >>> p = DigitalCall(60)
     >>> (p(60.00001),p(60),p(59.99999))
     (1, 0, 0)
     """
-
-    def __init__(self,k):
-        self._strike = k
-
-    def __call__(self,s):
-        if s > self._strike: return 1
+    def call(s):
+        if s > k: return 1
         else: return 0
+    return call
 
-class DigitalPut(Payoff):
+def DigitalPut(k):
     """
     >>> p = DigitalPut(60)
     >>> (p(60.00001),p(60),p(59.99999))
     (0, 0, 1)
     """
-
-    def __init__(self,k):
-        self._strike = k
-
-    def __call__(self,s):
-        if s < self._strike: return 1
+    def call(s):
+        if s < k: return 1
         else: return 0
+    return call
 
 
 def _test():
